45f5cfd136949e9b6345f34eb577b546fb55c311
Marcus_Gabriel <marcusdasilva19@outlook.com>
2024-05-02 18:40:45 -0300

    Criação da classe Calculadora para exemplificar como fazer documentação.

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+        String nome = args[0];
+        String sobrenome = args[1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+
+    }
+}
